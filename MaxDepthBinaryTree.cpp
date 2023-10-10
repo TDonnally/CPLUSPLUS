05b6a7c9d5950e249e9f67e3d26edd73fe375edc
unknown <tristandonnally@gmail.com>
2023-10-10 16:39:21 -0400

    added solution to MaxDepthBinaryTree.cpp

--- MaxDepthBinaryTree.cpp
+++ MaxDepthBinaryTree.cpp
@@ -30,11 +30,42 @@ struct TreeNode {
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
-        
+        //BASE
+        if(!root){
+            return 0;
+        }
+        int maxLeft = maxDepth(root->left);
+        int maxRight = maxDepth(root->right);
+        return max(maxLeft, maxRight) + 1;
     }
 };
 
 // main() is where program execution begins.
 int main() {
+    Solution s;
+
+    // Example 1: Create a binary tree [3, 9, 20, null, null, 15, 7]
+    TreeNode* root1 = new TreeNode(3);
+    root1->left = new TreeNode(9);
+    root1->right = new TreeNode(20);
+    root1->right->left = new TreeNode(15);
+    root1->right->right = new TreeNode(7);
+
+    // Calculate and log the maximum depth
+    int depth1 = s.maxDepth(root1);
+    cout << "Maximum Depth (Example 1): " << depth1 << endl;
+
+    // Example 2: Create a binary tree [1, null, 2]
+    TreeNode* root2 = new TreeNode(1);
+    root2->right = new TreeNode(2);
+
+    // Calculate and log the maximum depth
+    int depth2 = s.maxDepth(root2);
+    cout << "Maximum Depth (Example 2): " << depth2 << endl;
+
+    // Clean up allocated memory (important for preventing memory leaks)
+    delete root1;
+    delete root2;
+
     return 0;
 }
