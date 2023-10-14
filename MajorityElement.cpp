d258b2c12d8a4a5851c18b4ee5dc72a07953e582
unknown <tristandonnally@gmail.com>
2023-10-13 22:23:13 -0400

    solution to Majority Element

--- MajorityElement.cpp
+++ MajorityElement.cpp
@@ -26,7 +26,19 @@ using namespace std;
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        
+        int element=0;
+            int count=0;
+            for(int arr:nums){
+            if(count==0){
+                element=arr;
+            }
+            if(arr==element){
+                count++;
+            }
+            else
+                count--;
+        }
+        return element;
     }
 };
 
